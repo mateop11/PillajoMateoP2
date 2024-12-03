9a197322784b46e564c4fa916604a5dc3137a000
mateop11 <122656503+mateop11@users.noreply.github.com>
2024-12-03 07:25:12 -0500

    Agregar archivos de proyecto.

---
+++ PillajoMateoP2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PillajoMateoP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
